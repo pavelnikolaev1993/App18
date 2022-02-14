6c5f0923f6bef633ad4719d930caf314bcf552cd
Павел Николаев <pavelnikolaev1993@mail.ru>
2022-02-14 12:59:56 +0300

    <

--- Program.cs
+++ Program.cs
@@ -1,5 +1,37 @@
-﻿List<int> list = new List<int>();
-list.Add(1);
-list.Add(2);
-list.Add(3);
+﻿//Дана строка, содержащая скобки трёх видов (круглые, квадратные и фигурные) и любые другие символы. 
+//Проверить, корректно ли в ней расставлены скобки. Например, в строке ([]{ })[] скобки расставлены корректно, а в строке ([]] — нет.
+//Указание: задача решается однократным проходом по символам заданной строки слева направо; 
+//для каждой открывающей скобки в строке в стек помещается соответствующая закрывающая, 
+//каждая закрывающая скобка в строке должна соответствовать скобке из вершины стека (при этом скобка с вершины стека снимается); 
+//в конце прохода стек должен быть пуст.
+using System.Collections.Generic;
+Console.WriteLine("Введите строку");
+string str = Console.ReadLine();
+Console.WriteLine(Check(str)); 
 
+static bool Check(string str)
+{
+    Stack<char> stack = new Stack<char>();
+    Dictionary<char, char> sk = new Dictionary<char, char>
+{
+    {'(', ')' },
+    {'{', '}' },
+    {'[', ']' }
+};
+    foreach (char c in str)
+    {
+        if (c == '(' || c == '{' || c == '[')
+            stack.Push(sk[c]);
+        if (c == ')' || c == '}' || c == ']')
+        {
+            if (stack.Count == 0 || stack.Pop() != c)
+            {
+                return false;
+            }
+        }
+    }
+    if (stack.Count == 0)
+        return true;
+    else 
+        return false;
+}
